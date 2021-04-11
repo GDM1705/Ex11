edfe89787bbc900da9d51dcb52d516d132b6f9dd
GDM1705 <71228914+GDM1705@users.noreply.github.com>
2021-04-11 19:07:59 +0300

    Last

--- include/train.h
+++ include/train.h
@@ -1,30 +1,26 @@
-// Copyright 2021 Golovanov
+//  Copyright 2021 Golovanov
 
-#ifndef INCLUDE_TRAIN_H_
-#define INCLUDE_TRAIN_H_
-
+#pragma once
+#define TRAIN_H
+#ifdef TRAIN_H
+#include <iostream>
 class Cage {
-    bool light;
-
+    bool light;  
  public:
-    Cage* next;
-    Cage* prev;
+    Cage* next;  
+    Cage* prev;  
     Cage() : light(false), next(nullptr), prev(nullptr) {}
     void on() { light = true; }
     void off() { light = false; }
     bool get() const { return light; }
 };
-
 class Train {
- private:
-    Cage* first;
-    Cage* last;
-
+    Cage* first;  
+    Cage* last;   
  public:
     Train();
-    void addCage(bool);
-    void print() const;
-    unsigned int getLenght() const;
+    void addCageInTrain();
+    void printTrain();
+    unsigned int sumCage();
 };
-
-#endif  // INCLUDE_TRAIN_H_
+#endif  // TRAIN_H
