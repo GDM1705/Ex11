39232516513a287a47331c3b9ebe4867b3336293
GDM1705 <71228914+GDM1705@users.noreply.github.com>
2021-04-11 19:08:55 +0300

    zxcasdaqwe

--- src/train.cpp
+++ src/train.cpp
@@ -1,69 +1,46 @@
 //  Copyright 2021 Golovanov
 
 #include "train.h"
-
-#include <iostream>
-
 Train::Train() {
-    first = nullptr;
-    last = nullptr;
+    this->first = nullptr;
+    this->last = nullptr;
 }
-
-void Train::addCage(bool _light) {
+void Train::addCageInTrain() {
     Cage* new_cage = new Cage();
-
-    if (_light) {
-        new_cage->on();
-    }
-
-    if (first != nullptr) {
-        new_cage->prev = last;
-        last->next = new_cage;
-        last = new_cage;
-        new_cage->next = first;
-        first->prev = new_cage;
-    } else {
+    if (!first) {
         first = new_cage;
-        first->next = first;
-        first->prev = first;
-        last = first;
+        last = new_cage;
+        first->next = last;
+        first->prev = last;
     }
+    new_cage->prev = last;
+    new_cage->next = first;
+    last->next = new_cage;
+    last = new_cage;
 }
-
-void Train::print() const {
-    if (first != nullptr) {
-        Cage* cage = first;
-        while (cage != last) {
-            std::cout << cage->get() << std::endl;
-        }
-        std::cout << cage->get() << std::endl;
-    }
-}
-
-unsigned int Train::getLenght() const {
-    if (first == nullptr) {
-        return 0;
-    }
-
-    int steps = 0;
+void Train::printTrain() {
     Cage* cage = first;
-
-    cage->on();
-    while (true) {
-        ++steps;
+    int i = 1;
+    while (cage != last) {
+        std::cout << i << cage->get() << '\n';
         cage = cage->next;
-        while (cage->get() == false) {
-            ++steps;
+        i++;
+    }
+    std::cout << i << last->get() << '\n';
+}
+unsigned int Train::sumCage() {
+    int sum = 1;
+    Cage* cage = first;
+    first->off();
+    while (true) {
+        for (int i = 0; i < sum; i++) {
             cage = cage->next;
+            cage->on();
         }
-        cage->off();
-        for (size_t i = 0; i < steps; i++) {
+        for (int i = 0; i < sum; i++) {
             cage = cage->prev;
         }
-        if (cage->get() == false) {
-            return steps;
-        } else {
-            steps = 0;
-        }
+        if (cage->get()) return sum;
+        sum = sum+1;
     }
 }
